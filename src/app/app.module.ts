563a5404d7bdb189299f5987513d0384c858700e
WambiruL <wambirulorraine@gmail.com>
2021-05-23 12:12:36 +0300

    Creat search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchFormComponent,
 
   ],
   imports: [
