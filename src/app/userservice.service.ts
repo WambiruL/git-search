6fe783a909dd14ad58cb7ee2457b2b5f35d6caed
WambiruL <wambirulorraine@gmail.com>
2021-05-23 15:18:11 +0300

    Add the repos and user class blueprint to userservice

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Repos } from './repos';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviceService {
+  foundUser: User;
+  allRepos:Repos;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.foundUser=new User ("","","","",0,0,0,"", new Date);
+    this.allRepos= new Repos("","","",new Date,0,0,"");
+  }
 }
