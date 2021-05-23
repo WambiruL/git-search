1d7f8cb86335fcf86fec3708057935c24a777cc1
WambiruL <wambirulorraine@gmail.com>
2021-05-23 20:30:24 +0300

    Create the highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import {NgProgressHttpClientModule} from '@ngx-progressbar/http-client';
 import {NgProgressModule} from '@ngx-progressbar/core';
 import { DatePipe } from './date.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { DatePipe } from './date.pipe';
     RepositoriesComponent,
     UserComponent,
     SearchFormComponent,
-    DatePipe
+    DatePipe,
+    HighlightDirective
 
   ],
   imports: [
