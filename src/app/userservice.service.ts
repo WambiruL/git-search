ba576ddf686af3aba55dd1f32e5d07e2e4271ed2
WambiruL <wambirulorraine@gmail.com>
2021-05-23 15:27:15 +0300

    Add interface response to repository component

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -11,7 +11,22 @@ export class UserserviceService {
   allRepos:Repos;
 
   constructor(private http: HttpClient) {
-    this.foundUser=new User ("","","","",0,0,0,"", new Date);
+    this.foundUser=new User ("","","","",0,0,"", new Date);
     this.allRepos= new Repos("","","",new Date,0,0,"");
+  }
+
+  searchUser(searchName: string){
+
+    interface Response{
+      url:string;
+      login:string;
+      html_url:string;
+      location:string;
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
   }
 }
