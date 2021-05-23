630040050c0a4ea402261932bfbacf95edb4dd60
WambiruL <wambirulorraine@gmail.com>
2021-05-23 20:27:56 +0300

    Add the date calculation

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -5,8 +5,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ...args: any[]): any {
+      if(value){
+        const seconds=Math.floor((+ new Date()- +new Date(value))/1000);
+        if(seconds <29)
+        return 'Just now';
+        const intervals={
+          'year':31536000,
+          'month':259200,
+          'week':604800,
+          'day':86400,
+          'hour':3600,
+          'minute':60,
+          'second':1
+        };
+
+        let counter;
+        for(const i in intervals){
+          counter=Math.floor(seconds/intervals[i]);
+
+          if(counter >0)
+          if(counter===1){
+            return counter + ''+ i + 'ago';
+          }else{
+            return counter + '' + i + 's ago';
+          }
+        }
+      }
+    return value;
   }
 
 }
