392327ed5a26cadfa8ba93bd0752443bc950a88c
WambiruL <wambirulorraine@gmail.com>
2021-05-23 16:26:33 +0300

    Add the get repos functios and the searchs function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,3 +1,6 @@
+import { UserserviceService } from './../userservice.service';
+import { Repos } from './../repos';
+import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user: User;
+  repo: Repos;
 
-  constructor() { }
+  constructor(public myService:UserserviceService, private repoService:UserserviceService) {
+  }
+    searchs(searchName){
+      this.myService.searchUser(searchName).then(
+        (success)=>{
+          this.user=this.myService.foundUser;
+        },
+        (error)=>{
+          console.log(error)
+        }
+      );
+      this.repoService.getRepos(searchName).then(
+        (results)=>{
+          this.repo=this.repoService.allRepos
+          console.log(this.repo);
+        },
 
-  ngOnInit(): void {
+        (error)=>{
+          console.log(error);
+        }
+      );
+    }
+
+  ngOnInit(){
+    this.searchs('WambiruL')
   }
 
 }
