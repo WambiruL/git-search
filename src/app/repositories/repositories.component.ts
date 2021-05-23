a6dddab1a457893790e0dc70acfa6eba9704b872
WambiruL <wambirulorraine@gmail.com>
2021-05-23 14:58:31 +0300

    Add the repo blueprint to the repository component

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,3 +1,4 @@
+import { Repos } from './../repos';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  repo: Repos;
 
   constructor() { }
 
