400b1bab19b304ca156fb470d85ef2f3a86b8e13
WambiruL <wambirulorraine@gmail.com>
2021-05-23 19:58:10 +0300

    Add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user-profile", component:UserComponent},
+  {path:"search-repo", component:RepositoriesComponent},
+  {path:"", redirectTo:"/user-profile", pathMatch:"full"},
+  {path:"**", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
